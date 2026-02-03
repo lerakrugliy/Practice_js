104b44100aef00dccbc13b305038769acb7565da
lerakrugliy <lerakrugliy@gmail.com>
2026-02-03 20:08:21 +0200

    01

---
+++ js/01_variables.js
@@ -0,0 +1,22 @@
+const myName = "Lera";
+console.log(myName);
+
+const myAge = 13;
+console.log(myAge);
+
+console.log(typeof myName);
+
+console.log(typeof myAge);
+
+const productName = "cup";
+console.log(productName);
+
+let productPrice = 266;
+console.log(productPrice);
+
+productPrice = 277;
+
+console.log(productName);
+console.log(productPrice);
+
+//console.log(productName, productPrice );
